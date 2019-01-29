f0cc1ecc5f10510c78e5ea990dd9f876cddabeaa
Ogar616 <kamil.sobczyk@wp.pl>
2019-01-29 17:42:30 +0100

    unused imports removed

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import NavButton from "./navButton";
 import navLinks from "./data/navLinks";
