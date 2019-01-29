cc719717d40f93247a8f2b3d0910337550ee719c
Ogar616 <kamil.sobczyk@wp.pl>
2019-01-29 17:27:59 +0100

    hamburger fix

--- src/components/nav.js
+++ src/components/nav.js
@@ -59,7 +59,7 @@ class Nav extends Component {
               type="button"
               className="navbar-toggle collapsed"
               data-toggle="collapse"
-              // data-target="#bs-example-navbar-collapse-1"
+              data-target="#bs-example-navbar-collapse-1"
               aria-expanded="false"
             >
               <span className="sr-only">Toggle navigation</span>
