2543bbbb0ec82d76d9ebb268ea856588fe850c88
Ogar616 <kamil.sobczyk@wp.pl>
2019-02-03 09:51:43 +0100

    contact informations rwd upgraded

--- src/components/contactInformations.js
+++ src/components/contactInformations.js
@@ -4,14 +4,16 @@ class ContactInformations extends Component {
   render() {
     return (
       <div className="container contact">
-        <div className="col-md-4 col-md-offset-2">
-          Gabinet Psychologiczny i Psychoterapeutyczny Edyta Łuców-Stepczyńska
-          ul. Łopuskiego 50A/15 78-100 Kołobrzeg
-        </div>
-        <div className="col-md-4">
-          <a href="tel:+48505155252" />
-          505 155 252 psychoterapia.kolobrzeg@gmail.com <br /> NIP - 671 112 96
-          36 <br /> REGON - 330496250
+        <div className="row">
+          <div className="col-md-4 col-md-offset-2  contact1">
+            Gabinet Psychologiczny i Psychoterapeutyczny Edyta Łuców-Stepczyńska <br/>
+            ul. Łopuskiego 50A/15 78-100 Kołobrzeg
+          </div>
+          <div className="col-md-4">
+            <a href="tel:+48505155252" />
+            505 155 252 <br/> psychoterapia.kolobrzeg@gmail.com <br /> NIP - 671 112
+            96 36 <br /> REGON - 330496250
+          </div>
         </div>
       </div>
     );
